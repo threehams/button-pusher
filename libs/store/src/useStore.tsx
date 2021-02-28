91e6e5b45cf68aa2ae2378f5971f7c46825ffe30
David Edmondson <threehams@gmail.com>
2021-02-27 16:37:07 -0800

    try try again

--- libs/store/src/useStore.tsx
+++ libs/store/src/useStore.tsx
@@ -128,6 +128,10 @@ export const useStore = () => {
     playerDestination: undefined,
   }));
 
+  const purchasedUpgrades = useMemo(() => {
+    return Object.fromEntries(Object.entries(state.purchasedUpgradeMap));
+  }, [state.purchasedUpgradeMap]);
+
   const clearHistory = useCallback(() => {
     setState((draft) => {
       draft.messages = [];
@@ -582,3 +586,34 @@ const sortMethod = (upgrade: PurchasedUpgrade): SortMethod => {
   }
   return "horizontal";
 };
+
+type ValueOf<T> = T[keyof T];
+type UnionToIntersection<T> = (T extends T ? (p: T) => void : never) extends (
+  p: infer U,
+) => void
+  ? U
+  : never;
+type FromEntries<T extends readonly [PropertyKey, any]> = T extends T
+  ? Record<T[0], T[1]>
+  : never;
+type Flatten<T> = {} & {
+  [P in keyof T]: T[P];
+};
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface ObjectConstructor {
+    // keys: <T>(o: T) => Array<Extract<keyof T, string>>;
+    keys<T>(o: T): Array<Extract<keyof T, string>>;
+    entries<T extends { [key: string]: unknown }, K extends keyof T>(
+      o: T,
+    ): Array<[Extract<keyof T, string>, T[K]]>;
+    values<T extends { [key: string]: any }>(o: T): ValueOf<T>[];
+    fromEntries<
+      V extends PropertyKey,
+      T extends [readonly [V, any]] | Array<readonly [V, any]>
+    >(
+      entries: T,
+    ): Flatten<UnionToIntersection<FromEntries<T[number]>>>;
+  }
+}
