f64541c8eeb0c871b99d4cb1379662bd94a1b885
David Edmondson <threehams@gmail.com>
2021-03-06 17:51:08 -0800

    Add error boundary with reset button

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,4 +1,7 @@
+import { css } from "@emotion/react";
 import React, { useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Button } from "../components/Button";
 import { Game } from "../components/Game";
 
 export const Index = () => {
@@ -7,7 +10,71 @@ export const Index = () => {
     setOnClient(true);
   }, []);
 
-  return <>{onClient && <Game />}</>;
+  return (
+    <ErrorBoundary FallbackComponent={Reset}>
+      {onClient && <Game />}
+    </ErrorBoundary>
+  );
 };
 
 export default Index;
+
+const Reset = () => {
+  const [confirm, setConfirm] = useState(false);
+  const [reset, setReset] = useState(false);
+
+  useEffect(() => {
+    if (confirm && reset) {
+      localStorage.clear();
+      location.reload();
+    }
+  }, [confirm, reset]);
+  return (
+    <div
+      css={css`
+        width: 100%;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+      `}
+    >
+      <div
+        css={css`
+          max-width: 600px;
+        `}
+      >
+        <div>Something has gone very wrong. Try reloading the page.</div>
+        <div>If that doesn&apos;t work,</div>
+
+        <Button
+          css={css`
+            display: block;
+            width: 100%;
+            margin: 20px 0;
+          `}
+          onClick={() => {
+            setConfirm(true);
+          }}
+        >
+          click here to reset your game.
+        </Button>
+        <Button
+          css={css`
+            display: block;
+            width: 100%;
+            margin: 20px 0;
+            visibility: ${confirm ? "visible" : "hidden"};
+            pointer-events: ${confirm ? "auto" : "none"};
+          `}
+          onClick={() => {
+            setReset(true);
+          }}
+        >
+          click here to confirm.
+        </Button>
+      </div>
+    </div>
+  );
+};
